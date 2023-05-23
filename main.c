938ede05f82de204f7a88832e8c91225f1f6a196
Davi <123986101+Daviqrozz@users.noreply.github.com>
2023-05-23 18:51:25 -0300

    Esqueleto

--- main.c
+++ main.c
@@ -1,21 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 int main(void)
 {
 	int v=0;
 	char input;
+	char pedra='pedra';
+	char papel='papel';
+	char tesoura='tesoura';
+	
+	
+	
+	
 	while(v == 0){
 		printf("Pedra papel ou tesoura?");
 		scanf("%s",&input);
-		 
-		 //...
-		 
-		 
+		
+		//Sorteio da cpu
+		int bot = rand() % 3;  
+		printf("%d\n",bot);
+		
+		//Comparador de string
+		 if (strcmp(input, "pedra") == 1 || strcmp(input, "papel") == 0 || strcmp(input, "tesoura") == 0) {
+           jogador = 1;
+           printf("%d",&jogador);
+			}
+        
+        
+        
+        //Encerrar o loop
 		printf("Deseja jogar denovo?(0=sim,1=nao): ");
         scanf("%d", &v);
-		
-	
 	}
-		
+	printf("PROGRAMA ENCERRADO!")
+	;
 	return 0;
 }
