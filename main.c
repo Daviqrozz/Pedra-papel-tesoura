05c32b23016d78a4d492a7974e89783f1476b2b7
Davi <123986101+Daviqrozz@users.noreply.github.com>
2023-05-24 21:43:53 -0300

    Codigo final
    Codigo concluido

--- main.c
+++ main.c
@@ -7,86 +7,99 @@
 
 int main(void)
 {
-	char valor[MAX_LENGTH]="sim";
-
-	char input[MAX_LENGTH];
-	int jogador;
-	char cpu[MAX_LENGTH];
-	
-	 srand(time(NULL));
-	
-	//1 = Pedra
-	//2 = papel
-	//3 = Tesoura
-	 valor[strcspn(input, "\n")] = '\0';
-	
-	while(strcmp(valor,"sim")){
-	printf("Pedra,Papel ou Tesoura?");
-    fgets(input, MAX_LENGTH, stdin);
-
-
-    input[strcspn(input, "\n")] = '\0';
-
-    if (strcmp(input, "pedra") == 0) {
-        printf("Você escolheu pedra.\n");
-        jogador = 1;
-      
-    } else if (strcmp(input, "papel") == 0) {
-        printf("Você escolheu papel.\n");
-        jogador = 2;
-       
-    } else if (strcmp(input, "tesoura") == 0) {
-        printf("Você escolheu tesoura.\n");
-        jogador = 3;
-      
-    } else {
-        printf("Opção inválida.\n");
-        break;
-    }
+    char valor[MAX_LENGTH] = "sim";
+    char input[MAX_LENGTH];
+    int jogador;
+    char cpu[MAX_LENGTH];
     
-		//Sorteio da cpu
-		int bot = rand() % 3 + 1;  
-		
-		//Logica do jogo
-		if(bot == jogador ){
-		    printf("Empate\n");
-		}
-		else if(bot == 1 && jogador == 2){
-		    printf("CPU escolheu Pedra\n");
-		    printf("Você perdeu!\n");
-		}
-		else if (bot == 1 && jogador == 3){
-		    printf("CPU escolheu Pedra\n");
-		    printf("Você perdeu!\n");
-		}
-		else if (bot == 2 && jogador == 1){
-		    printf("CPU escolheu Papel\n");
-		    printf("Você ganhou!\n");
-		}
-		else if (bot == 2 && jogador == 3){
-		    printf("CPU escolheu Papel\n");
-		    printf("Você ganhou!\n");
-		}
-		else if (bot == 3 && jogador == 1){
-		    printf("CPU escolheu Tesoura\n");
-		    printf("Você perdeu!\n");
-		}
-		else if (bot == 3 && jogador == 2){
-		    printf("CPU escolheu Tesoura\n");
-		    printf("Você ganhou!\n");
-		}
-		
-        //Encerrar o loop
-		printf("Deseja jogar denovo?(sim ou nao): ");
-		
-		fgets(valor, MAX_LENGTH, stdin);
-		valor[strcspn(input, "\n")] = '\0';
-		if (valor != "sim")
-		break;
+    srand(time(NULL));
+    
+    //1 = Pedra
+    //2 = papel
+    //3 = Tesoura
+    valor[strcspn(valor, "\n")] = '\0';
+    
+    //Inicio do loop
+    while(strcmp(valor, "sim") == 0)
+    {
+        //Escolha do usuario
+        printf("Pedra, Papel ou Tesoura? ");
+        //Armazenando a escolha do usuario na variavel input
+        fgets(input, MAX_LENGTH, stdin);
+        //Sanitizando a escolha para ficar 'pura'
+        input[strcspn(input, "\n")] = '\0';
+        
+        //definindo a numeraçao de cada escolha
+        if (strcmp(input, "pedra") == 0)
+        {
+            printf("Você escolheu pedra.\n");
+            jogador = 1;
+        }
+        else if (strcmp(input, "papel") == 0)
+        {
+            printf("Você escolheu papel.\n");
+            jogador = 2;
+        }
+        else if (strcmp(input, "tesoura") == 0)
+        {
+            printf("Você escolheu tesoura.\n");
+            jogador = 3;
+        }
+        else
+        {
+            printf("Opção inválida.\n");
+            continue;
         }
         
+        // Sorteio da cpu
+        int bot = rand() % 3 + 1;
         
-    printf("PROGRAMA ENCERRADO!");
+        // Lógica do jogo
+        //Impressão da CPU
+        if (bot == jogador)
+        {
+            printf("Empate\n");
+        }
+        else if (bot == 1 && jogador == 2)
+        {
+            printf("CPU escolheu Pedra\n");
+            printf("Você perdeu!\n");
+        }
+        else if (bot == 1 && jogador == 3)
+        {
+            printf("CPU escolheu Pedra\n");
+            printf("Você perdeu!\n");
+        }
+        else if (bot == 2 && jogador == 1)
+        {
+            printf("CPU escolheu Papel\n");
+            printf("Você ganhou!\n");
+        }
+        else if (bot == 2 && jogador == 3)
+        {
+            printf("CPU escolheu Papel\n");
+            printf("Você ganhou!\n");
+        }
+        else if (bot == 3 && jogador == 1)
+        {
+            printf("CPU escolheu Tesoura\n");
+            printf("Você perdeu!\n");
+        }
+        else if (bot == 3 && jogador == 2)
+        {
+            printf("CPU escolheu Tesoura\n");
+            printf("Você ganhou!\n");
+        }
+        
+        // Encerrar o loop
+        printf("Deseja jogar novamente? (sim ou nao): ");
+        fgets(valor, MAX_LENGTH, stdin);
+        valor[strcspn(valor, "\n")] = '\0';
+        if (strcmp(valor, "nao") == 0)
+        {
+            printf("PROGRAMA ENCERRADO!\n");
+            break;
+        }
+    }
     return 0;  
-	}
-
+}
